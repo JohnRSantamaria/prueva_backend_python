a602e1bd45b7a617cba07e6f2df1dda0ec09174a
John Santamaria <36826659+JohnRSantamaria@users.noreply.github.com>
2023-06-26 16:44:04 -0500

    deployment2

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets
 from .serializer import TaskSerializer
 from .models import Taks
+from django.http import HttpResponse
 # Create your views here.
 class TaskView(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     queryset = Taks.objects.all()
+
+def login_view(request):
+    return HttpResponse("<h1>Deployed</h1>")
